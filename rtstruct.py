b39a56086e50d8395ab8c94a57f0343b087b46f1
JY Lee <52771148+xdrcftv@users.noreply.github.com>
2019-11-04 22:20:55 +0900

    Add files via upload

--- rtstruct.py
+++ rtstruct.py
@@ -15,12 +15,20 @@ root = xml_doc.getroot()
 
 struct_dir = root.findtext('RTStructure')
 RT_struct = dcm.dcmread(struct_dir)
+print(type(RT_struct))
 ROI_contour = RT_struct.ROIContourSequence
 
 tumor_ROI = xml_parser.tumor_ROINo(root)
 
 tumor1 = ROI_contour[3]
-print(tumor1)
+
+print(list(tumor1._dict.items()))
+print(tumor1[(0x3006, 0x0040)])
+#print(tumor1.ContourSequence[0].ContourData)
+
+#for i in range(len(tumor1.ContourSequence)):
+#                UID = tumor1.ContourSequence[i].ContourImageSequence[0].ReferencedSOPInstanceUID
+#                reference_list = np.append(reference_list, UID)
 # for i in tumor_ROI:
 #     print(ROI_contour[i-1])
 
